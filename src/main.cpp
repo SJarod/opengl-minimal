4b71189b90055257ef332786b7b7195cae8b8f37
Jarod Sengkeo <jarsengkeoPro@gmail.com>
2022-09-14 22:58:38 +0200

    compiling and linking shaders

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,10 @@
 
 void windowInit();
 void openglInit();
+void openglCompileShaders();
 
 GLFWwindow* window;
+GLuint shaderProgram;
 
 const char* vstriangleSrc = R"GLSL(
 #version 450
@@ -27,8 +29,8 @@ vec3 colors[3] = {
 
 void main()
 {
-	gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-	vertexColor = colors[gl_VertexIndex];
+	gl_Position = vec4(positions[gl_VertexID], 0.0, 1.0);
+	vertexColor = colors[gl_VertexID];
 }
 )GLSL";
 
@@ -62,6 +64,40 @@ void openglInit()
 		throw std::exception("Failed to load OpenGL functions");
 }
 
+void openglCompileShaders()
+{
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vstriangleSrc, NULL);
+	glCompileShader(vs);
+
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fstriangleSrc, NULL);
+	glCompileShader(fs);
+
+	int success[2];
+	char infoLog[2][512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &success[0]);
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &success[1]);
+
+	if (!success[0] || !success[1])
+	{
+		glGetShaderInfoLog(vs, 512, NULL, infoLog[0]);
+		std::cout << infoLog[0] << std::endl;
+		glGetShaderInfoLog(fs, 512, NULL, infoLog[1]);
+		std::cout << infoLog[1] << std::endl;
+
+		throw std::exception("Failed to compile shaders");
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+}
+
 int main()
 {
 	try
@@ -70,6 +106,7 @@ int main()
 
 		windowInit();
 		openglInit();
+		openglCompileShaders();
 
 		while (!glfwWindowShouldClose(window))
 		{
