9277184d17a20f20f29822a4bfc2a437567ce7b8
Jarod Sengkeo <jarsengkeoPro@gmail.com>
2022-09-14 21:59:47 +0200

    first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+GLFWwindow* window;
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	window = glfwCreateWindow(800, 600, "OpenGL window", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
 	return EXIT_SUCCESS;
 }
