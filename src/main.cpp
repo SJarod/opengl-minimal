837828602e615543d4193bf96d91c3ed4aa2fcc1
Jarod <jarsengkeoPro@gmail.com>
2024-02-11 00:45:43 +0100

    as header-only lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,4 @@
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
-
-
-
-void init_wsi();
-void terminate_wsi();
-
-void create_window();
-void destroy_window();
-
-void opengl_load_symbols();
-
-void opengl_create_vertex_buffer();
-void opengl_create_vertex_array();
-
-void opengl_compile_shaders();
-
-void draw_frame();
-
-
-GLFWwindow* window;
-GLuint VBO;
-GLuint VAO;
-GLuint shaderProgram;
+#include "opengl_tutorial.hpp"
 
 
 float vertices[] = {
@@ -63,123 +37,44 @@ void main()
 )GLSL";
 
 
-void create_window()
-{
-	// OpenGL core 4.6
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	
-	window = glfwCreateWindow(800, 600, "OpenGL window", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
-}
-
-void opengl_load_symbols()
-{
-	if (!gladLoadGL(glfwGetProcAddress))
-		throw std::exception("Failed to load OpenGL functions");
-}
-
-void opengl_create_vertex_buffer()
-{
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-}
-
-void opengl_create_vertex_array()
-{
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-}
-
-void opengl_compile_shaders()
-{
-	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vs, 1, &vstriangleSrc, NULL);
-	glCompileShader(vs);
-
-	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fs, 1, &fstriangleSrc, NULL);
-	glCompileShader(fs);
-
-	int success[2];
-	char infoLog[2][512];
-	glGetShaderiv(vs, GL_COMPILE_STATUS, &success[0]);
-	glGetShaderiv(vs, GL_COMPILE_STATUS, &success[1]);
-
-	if (!success[0] || !success[1])
-	{
-		glGetShaderInfoLog(vs, 512, NULL, infoLog[0]);
-		std::cout << infoLog[0] << std::endl;
-		glGetShaderInfoLog(fs, 512, NULL, infoLog[1]);
-		std::cout << infoLog[1] << std::endl;
-
-		throw std::exception("Failed to compile shaders");
-	}
-
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vs);
-	glAttachShader(shaderProgram, fs);
-	glLinkProgram(shaderProgram);
-
-	glDeleteShader(vs);
-	glDeleteShader(fs);
-}
-
-void draw_frame()
-{
-	glViewport(0, 0, 800, 600);
-
-	glClearColor(0.2f, 0.2f, 0.2f, 1.f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glUseProgram(shaderProgram);
-	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glBindVertexArray(0);
-}
-
 int main()
 {
 	// TODO : ubo (mvp)
 	// TODO : texture
+
+	GLuint vbo;
+	GLuint vao;
+	GLuint shaderProgram;
 	
 	try
 	{
-		glfwInit();
+		init_wsi();
 
-		create_window();
+		GLFWwindow* window = create_window();
+
 		opengl_load_symbols();
-		opengl_create_vertex_buffer();
-		opengl_create_vertex_array();
-		opengl_compile_shaders();
+
+		opengl_create_vertex_buffer(vbo, vertices, sizeof(vertices));
+		opengl_create_vertex_array(vao, vbo);
+		opengl_compile_shaders(shaderProgram, vstriangleSrc, fstriangleSrc);
 
 		while (!glfwWindowShouldClose(window))
 		{
 			glfwSwapBuffers(window);
 			glfwPollEvents();
-			draw_frame();
+
+			draw_frame(shaderProgram, vao);
 		}
+
+		destroy_window(window);
+
+		terminate_wsi();
 	}
 	catch (const std::exception& ex)
 	{
 		std::cerr << ex.what() << std::endl;
 		return EXIT_FAILURE;
 	}
-
-	glfwDestroyWindow(window);
-
-	glfwTerminate();
-
+	
 	return EXIT_SUCCESS;
 }
